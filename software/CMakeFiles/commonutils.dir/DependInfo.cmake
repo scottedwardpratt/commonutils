f7c7d10c085e65ba1a8a70fde67d72a905e672fe
Scott Pratt <scott@pratt-linux.nscl.msu.edu>
2020-03-22 18:23:21 -0400

    nothing really but CMakeFiles crap

--- software/CMakeFiles/commonutils.dir/DependInfo.cmake
+++ software/CMakeFiles/commonutils.dir/DependInfo.cmake
@@ -4,31 +4,30 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/scottpratt/git/commonutils/software/src/Arrays/arraycalc.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/arraycalc.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/Arrays/charray.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/charray.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/Arrays/mclist.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/mclist.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/Arrays/mcprlist.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/mcprlist.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/Arrays/threed.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/threed.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/Arrays/ylmarray.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/ylmarray.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/GSLMatrix/gslmatrix.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/GSLMatrix/gslmatrix.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/Misc/misc.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/Misc/misc.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/NumMath/randy.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/NumMath/randy.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/ParameterMap/parametermap.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/ParameterMap/parametermap.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/Qualifiers/qualifier.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/Qualifiers/qualifier.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/SpecialFunctions/Bessel/bess.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/Bessel/bess.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/SpecialFunctions/CartesianHarmonics/chcalc.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/CartesianHarmonics/chcalc.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/SpecialFunctions/CoulWave/coulwave.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/CoulWave/coulwave.cc.o"
-  "/Users/scottpratt/git/commonutils/software/src/SpecialFunctions/Ylm/ylm.cc" "/Users/scottpratt/git/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/Ylm/ylm.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/Arrays/arraycalc.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/arraycalc.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/Arrays/charray.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/charray.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/Arrays/mclist.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/mclist.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/Arrays/mcprlist.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/mcprlist.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/Arrays/threed.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/threed.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/Arrays/ylmarray.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/ylmarray.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/GSLMatrix/gslmatrix.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/GSLMatrix/gslmatrix.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/Misc/misc.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Misc/misc.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/NumMath/randy.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/NumMath/randy.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/ParameterMap/parametermap.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/ParameterMap/parametermap.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/Qualifiers/qualifier.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Qualifiers/qualifier.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/SpecialFunctions/Bessel/bess.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/Bessel/bess.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/SpecialFunctions/CartesianHarmonics/chcalc.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/CartesianHarmonics/chcalc.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/SpecialFunctions/CoulWave/coulwave.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/CoulWave/coulwave.cc.o"
+  "/home/scott/git/commonutils/commonutils/software/src/SpecialFunctions/Ylm/ylm.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/Ylm/ylm.cc.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "include"
-  "/Users/scottpratt/git/hp/alice/software/commonutils/include"
-  "/Users/scottpratt/git/hp/alice/software/b3d2/include"
-  "/usr/local/Cellar/gsl/2.6/include"
-  "/usr/local/include/eigen3"
+  "/hp/alice/software/commonutils/include"
+  "/hp/alice/software/b3d2/include"
+  "/usr/include/eigen3"
   )
 
 # Targets to which this target links.
