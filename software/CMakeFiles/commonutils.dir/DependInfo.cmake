898597a16811ff22a3490e7c0f22abf33f16419b
Scott Pratt <scott@pratt-linux.nscl.msu.edu>
2020-04-01 21:01:24 -0400

    no real changes

--- software/CMakeFiles/commonutils.dir/DependInfo.cmake
+++ software/CMakeFiles/commonutils.dir/DependInfo.cmake
@@ -4,23 +4,23 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/scott/git/commonutils/commonutils/software/src/Arrays/arraycalc.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/arraycalc.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/Arrays/charray.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/charray.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/Arrays/mclist.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/mclist.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/Arrays/mcprlist.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/mcprlist.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/Arrays/threed.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/threed.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/Arrays/ylmarray.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/ylmarray.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/GSLMatrix/gslmatrix.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/GSLMatrix/gslmatrix.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/Misc/misc.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Misc/misc.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/NumMath/randy.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/NumMath/randy.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/ParameterMap/parametermap.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/ParameterMap/parametermap.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/Qualifiers/qualifier.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/Qualifiers/qualifier.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/SpecialFunctions/Bessel/bess.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/Bessel/bess.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/SpecialFunctions/CartesianHarmonics/chcalc.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/CartesianHarmonics/chcalc.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/SpecialFunctions/CoulWave/coulwave.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/CoulWave/coulwave.cc.o"
-  "/home/scott/git/commonutils/commonutils/software/src/SpecialFunctions/Ylm/ylm.cc" "/home/scott/git/commonutils/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/Ylm/ylm.cc.o"
+  "/home/scott/git/commonutils/software/src/Arrays/arraycalc.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/arraycalc.cc.o"
+  "/home/scott/git/commonutils/software/src/Arrays/charray.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/charray.cc.o"
+  "/home/scott/git/commonutils/software/src/Arrays/mclist.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/mclist.cc.o"
+  "/home/scott/git/commonutils/software/src/Arrays/mcprlist.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/mcprlist.cc.o"
+  "/home/scott/git/commonutils/software/src/Arrays/threed.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/threed.cc.o"
+  "/home/scott/git/commonutils/software/src/Arrays/ylmarray.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/Arrays/ylmarray.cc.o"
+  "/home/scott/git/commonutils/software/src/GSLMatrix/gslmatrix.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/GSLMatrix/gslmatrix.cc.o"
+  "/home/scott/git/commonutils/software/src/Misc/misc.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/Misc/misc.cc.o"
+  "/home/scott/git/commonutils/software/src/NumMath/randy.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/NumMath/randy.cc.o"
+  "/home/scott/git/commonutils/software/src/ParameterMap/parametermap.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/ParameterMap/parametermap.cc.o"
+  "/home/scott/git/commonutils/software/src/Qualifiers/qualifier.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/Qualifiers/qualifier.cc.o"
+  "/home/scott/git/commonutils/software/src/SpecialFunctions/Bessel/bess.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/Bessel/bess.cc.o"
+  "/home/scott/git/commonutils/software/src/SpecialFunctions/CartesianHarmonics/chcalc.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/CartesianHarmonics/chcalc.cc.o"
+  "/home/scott/git/commonutils/software/src/SpecialFunctions/CoulWave/coulwave.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/CoulWave/coulwave.cc.o"
+  "/home/scott/git/commonutils/software/src/SpecialFunctions/Ylm/ylm.cc" "/home/scott/git/commonutils/software/CMakeFiles/commonutils.dir/src/SpecialFunctions/Ylm/ylm.cc.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- software/cmake_install.cmake
+++ software/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/scott/git/commonutils/commonutils/software
+# Install script for directory: /home/scott/git/commonutils/software
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/scott/git/commonutils/commonutils/software/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/scott/git/commonutils/software/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
