6802839e19c5201c5567945f3f9355971eb6e8de
scottedwardpratt <scott@pratt-linux.nscl.msu.edu>
2020-12-30 11:06:41 -0500

    lots of changes to qualifier stuff

--- software/include/qualifier.h
+++ software/include/qualifier.h
@@ -4,14 +4,23 @@
 
 using namespace std;
 
+class CQualifier{
+public:
+	int npars;
+	string qualname;
+	vector<string> type;
+	vector<string> parname;
+	vector<string> value;	
+};
+
 class CQualifiers{
 public:
+	vector<CQualifier *> qualifier;
 	int nqualifiers;
-	string qualifier[100];
-	int npars[100];
-	string type[100][10],parname[100][10],value[100][10];
 	void Read(string qfilename);
 	void SetPars(CparameterMap *pmap,int iqualifier);
+	void Print();
 };
+
 
 #endif
