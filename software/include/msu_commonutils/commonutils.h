7e46ed0fb253d90e021cb68ecbed36b6b7cb9fcc
scottedwardpratt <prattsc@msu.edu>
2022-04-28 17:34:20 -0400

    dummy message

---
+++ software/include/msu_commonutils/commonutils.h
@@ -0,0 +1,32 @@
+#ifndef __CORAL_UTILITIES_H_INCLUDE__
+#define __CORAL_UTILITIES_H_INCLUDE__
+// This includes all headers
+//#define __B3D_USE_HDF5__
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <fstream>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_sf.h>
+//#include <eigen3/Eigen/Core>
+
+//typedef double FourVector[4];
+//typedef Eigen::Matrix<double,4,4> FourTensor;
+//typedef Eigen::Matrix<double,4,1> FourVector;
+
+#include "arrays.h"
+#include "commondefs.h"
+#include "constants.h"
+#include "gslmatrix.h"
+#include "misc.h"
+#include "parametermap.h"
+#include "qualifier.h"
+#include "randy.h"
+#include "sf.h"
+#include "log.h"
+
+#endif
